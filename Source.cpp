9e28850d6c0e76fe12f30fcfc5298ae676b60880
Shaina Karasik <shainanihon@gmail.com>
2024-08-18 12:43:12 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,7 @@
 //coding exercise page 26 of C++ code Quickly
 int main()
 {
-	// We are calrifying the exact size of different Variable based on their var type. using the sizeof(operator).
+	// We are clarifying the exact size of different variables based on their var type. using the sizeof(operator).
 	std::cout << "We will display here the size of several variables here in bytes" << std::endl;
 	std::cout << " ---------------------------------------------------------------" << std::endl;
 
@@ -12,13 +12,18 @@ int main()
 	std::cout << "The byte size of the variable char is  " << sizeof(long) << std::endl;
 	std::cout << "The byte size of the variable char is  " << sizeof(long long) << std::endl;
 	std::cout << "The byte size of the variable char is  " << sizeof(double) << std::endl;
-	//note we did not do short or strings? 	std::cout << "The byte size of the variable char is  " << sizeof(char) << std::endl;
+	//note we did not do short or strings? 	
+	std::cout << "The byte size of the variable char is  " << sizeof(char) << std::endl;
 	//nor did we do float type strings
 	//i want to create a list of ALL variable types in C++ and maybe actually be motivate to create C++ app with all types and all concetps
 	//explained in a easy way from beg to advanced with projects in between for games, game engines, software, ceoncept design for game making software,
 	//game assets software
-	//blender software
+	// Blender software
 	//adobe software
+	// Maya software
+	// Unreal Engine 5
+	// Unity
+	// Game engines
 
 	
 
